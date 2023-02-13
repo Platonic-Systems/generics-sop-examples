8ecf7112b14bf1615ee124145e8e48f2840c833a
Sridhar Ratnakumar <srid@srid.ca>
2023-02-13 16:03:18 -0500

    Modernize flake.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    nixpkgs-140774-workaround.url = "github:srid/nixpkgs-140774-workaround";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -10,25 +11,9 @@
       imports = [ inputs.haskell-flake.flakeModule ];
       perSystem = { self', pkgs, ... }: {
         haskellProjects.default = {
-          packages.generics-sop-examples.root = ./.;
-          buildTools = hp:
-            let
-              # Workaround for https://github.com/NixOS/nixpkgs/issues/140774
-              fixCyclicReference = drv:
-                pkgs.haskell.lib.overrideCabal drv (_: {
-                  enableSeparateBinOutput = false;
-                });
-            in
-            {
-              ghcid = fixCyclicReference hp.ghcid;
-              haskell-language-server = hp.haskell-language-server.overrideScope
-                (lself: lsuper: {
-                  ormolu = fixCyclicReference hp.ormolu;
-                });
-
-              pandoc = pkgs.pandoc;
-              texlive = pkgs.texlive.combined.scheme-full;
-            };
+          imports = [
+            inputs.nixpkgs-140774-workaround.haskellFlakeProjectModules.default
+          ];
         };
         packages.default = self'.packages.generics-sop-examples;
       };
